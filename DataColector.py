f0c9b9447327b259c00f7700a6b268b4f7fcf197
Justin Boileau <jzb.tuber@gmail.com>
2026-04-13 17:42:56 -0400

    Added the main .py file

---
+++ DataColector.py
@@ -0,0 +1,6 @@
+#----------------------------------------------------------------#
+# This code was writen by Justin Boileau
+# for the PIAHNO reseach project at the "Université de Montreal".
+# This code is free to use to anyone as said by the License.
+# This is the main .py file of the app.
+#----------------------------------------------------------------#
